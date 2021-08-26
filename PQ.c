d55f416a478eb587be0da5878fe77e78f765e8cb
Elias Pereira Gomes Junior <eliasepgj@gmail.com>
2021-08-25 23:09:13 -0300

    retirando a variavel global do codigo do heap

--- PQ.c
+++ PQ.c
@@ -2,79 +2,84 @@
 #include <stdlib.h>
 #include "PQ.h"
 
-static Item *pq;
-static int *map;
-static int N;
+struct wrapper{
+    Item *pq;
+    int *map;
+    int N;
+};
 
-static void swap(int i, int j) {
-    exch(pq[i], pq[j]);
-    map[id(pq[i])] = i;
-    map[id(pq[j])] = j;
+void swap(int i, int j,Wrapper* wrapper) {
+    exch(wrapper->pq[i], wrapper->pq[j]);
+    wrapper->map[id(wrapper->pq[i])] = i;
+    wrapper->map[id(wrapper->pq[j])] = j;
 }
 
-void fix_up(Item *a, int k) {
-    while (k > 1 && more(a[k/2], a[k])) {
-        swap(k, k/2);
+void fix_up(int k, Wrapper* wrapper) {
+    Item *a = wrapper->pq;
+    while (k > 1 && more(a[k/2], a[k])){
+        swap(k, k/2, wrapper);
         k = k/2;
     }
 }
 
-void fix_down(Item *a, int sz, int k){
-  while (2*k <= sz) {
-    int j = 2*k;
-    if (j < sz && more(a[j], a[j+1])){
-      j++;
+void fix_down(int k, Wrapper* wrapper){
+    Item *a = wrapper->pq;
+    int sz = wrapper->N;
+    while (2*k <= sz) {
+        int j = 2*k;
+        if (j < sz && more(a[j], a[j+1])){
+        j++;
+        }
+        if (!more(a[k], a[j])) {
+        break;
+        }
+        swap(k, j, wrapper);
+        k = j;
     }
-    if (!more(a[k], a[j])) {
-      break;
-    }
-    swap(k, j);
-    k = j;
-  }
 }
 
-void PQ_init(int maxN) {
-    pq = (Item *) malloc((maxN+1) * sizeof (Item));
-    map = (int *) malloc((maxN+1) * sizeof (int));
-    N = 0;
+Wrapper* PQ_init(int maxN) {
+    Wrapper* wrapper = (Wrapper*)malloc(sizeof(Wrapper));
+    wrapper->pq = (Item *) malloc((maxN+1) * sizeof (Item));
+    wrapper->map = (int *) malloc((maxN+1) * sizeof (int));
+    wrapper->N = 0;
 } 
 
-void PQ_insert(Item v) {
-    N++;
-    pq[N] = v;
-    map[id(v)] = N;
-    fix_up(pq, N);
+void PQ_insert(Item v, Wrapper* wrapper) {
+    wrapper->N++;
+    wrapper->pq[wrapper->N] = v;
+    wrapper->map[id(v)] = wrapper->N;
+    fix_up(wrapper->N, wrapper);
 }
 
-Item PQ_delmin() {
-    Item min = pq[1];
-    swap(1, N);
-    N--;
-    fix_down(pq, N, 1);
+Item PQ_delmin(Wrapper* wrapper) {
+    Item min = wrapper->pq[1];
+    swap(1, wrapper->N, wrapper);
+    wrapper->N--;
+    fix_down(1, wrapper);
     return min;
 }
 
-Item PQ_min() {
-    return pq[1];
+Item PQ_min(Wrapper* wrapper) {
+    return wrapper->pq[1];
 }
 
-void PQ_decrease_key(int id, double value) {
-    int i = map[id];
-    value(pq[i]) = value;
-    fix_up(pq, i);
+void PQ_decrease_key(int id, double value, Wrapper* wrapper) {
+    int i = wrapper->map[id];
+    value(wrapper->pq[i]) = value;
+    fix_up(i, wrapper);
 }
 
-bool PQ_empty() {
-    return N == 0;
+bool PQ_empty(Wrapper* wrapper) {
+    return wrapper->N == 0;
 }
 
-int  PQ_size() {
-    return N;
+int  PQ_size(Wrapper* wrapper) {
+    return wrapper->N;
 }
 
-void PQ_finish() {
-    free(pq);
-    free(map);
+void PQ_finish(Wrapper* wrapper) {
+    free(wrapper->pq);
+    free(wrapper->map);
+    free(wrapper);
 }
-
-
--- PQ.h
+++ PQ.h
@@ -4,13 +4,20 @@
 #include <stdbool.h>
 #include "item.h"
 
-void PQ_init(int maxN); // cria uma min PQ com capacidade maxN
-void PQ_insert(Item);   // Insere Item na PQ. A maior prioridade é a do Item com menor campo 'value'
-Item PQ_delmin();       // Remove Item com menor campo 'value' da PQ
-void PQ_decrease_key(int id, double value); // Muda a prioridade do nó com identificador 'id' para 'value'
-Item PQ_min();          // Retorna Item com menor campo 'value' da PQ
-bool PQ_empty();        // Retorna True se a PQ não tem elementos
-int  PQ_size();         // Número de elementos na PQ
-void PQ_finish();       // Libera memória
+typedef struct wrapper Wrapper;
+
+Item get_item(Wrapper* wrapper, int index);
+void set_map(Wrapper* wrapper, int index, int value);
+
+Wrapper* PQ_init(int maxN); // cria uma min PQ com capacidade maxN
+void PQ_insert(Item v, Wrapper* wrapper);   // Insere Item na PQ. A maior prioridade é a do Item com menor campo 'value'
+Item PQ_delmin(Wrapper* wrapper);       // Remove Item com menor campo 'value' da PQ
+void PQ_decrease_key(int id, double value, Wrapper* wrapper); // Muda a prioridade do nó com identificador 'id' para 'value'
+Item PQ_min(Wrapper* wrapper);          // Retorna Item com menor campo 'value' da PQ
+void fix_down(int k, Wrapper* wrapper);
+void fix_up(int k, Wrapper* wrapper);
+bool PQ_empty(Wrapper* wrapper);        // Retorna True se a PQ não tem elementos
+int  PQ_size(Wrapper* wrapper);         // Número de elementos na PQ
+void PQ_finish(Wrapper* wrapper);       // Libera memória
 
 #endif
