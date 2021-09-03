cfa55caadc67159faf2a4e56d28f04e960e76b4e
Igor Dummer <32550876+IgorDummer@users.noreply.github.com>
2021-09-03 19:42:32 -0300

    Consertando erro de português

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
   Data *data = read_input_file(argv[1]);            /* Lê os dados de entrada e armazena em data */
   Statistics *statistics = create_statistics(data); /* Aloca memoria pra struct Statistics */
   calculate_distances(statistics, data);            /* Calcula as distancias e armazena nos RTT's */
-  calculate_inflaction(statistics, data);           /* Faz o calcula da inflacao entre os vertices */
+  calculate_inflaction(statistics, data);           /* Faz o calculo da inflacao entre os vertices */
   output_statistics(statistics, argv[2]);           /* Escreve os dados calculados em um arquivo de saida */
   destroy_statistics(statistics);                   /* Libera toda a memoria alocada */
   return 0;
